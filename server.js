5861f6a1d67765a2eb6af4c97f78df1e602038a6
giraldiego <giraldiego@gmail.com>
2021-10-29 11:54:49 -0500

    Add connection to mongoose DB

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express');
+// Load enviroment variables from .env file
+require('dotenv').config();
 
 // Creates server app
+const express = require('express');
 const app = express();
 
+// Add middleware to handle json format
+app.use(express.json)
+
+// Database connection
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Connected to Database!');
+    })
+    .catch((err) => {
+        console.log('Failed to connect:', err.message);
+    });
+
+// Add listener for errors in DB Connection
+const db = mongoose.connection;
+db.on('error', err => console.log('Error:', err.message));
+
 app.get('/', (req, res) => {
-    res.send('Welcome to my express server!')
+    res.send('Welcome to my express server!');
 })
 
 app.listen(3000, () => {
